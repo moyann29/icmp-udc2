dce4848f2bc4fa3b2ae14bc28a84914afddcf6d7
moyann29 <moyann29@gmail.com>
2026-07-04 12:21:18 +0800

    Delete icmp-udc2-bof directory

--- icmp-udc2-bof/udc2.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-typedef int(*UDC2ProxyCall)(const char* sendBuf, int sendBufLen, char* recvBuf, int recvBufMaxLen);
-typedef void(*UDC2ProxyClose)();
-
-typedef struct _UDC2_INFO {
-	DWORD version;
-	UDC2ProxyCall proxyCall;
-	UDC2ProxyClose proxyClose;
-} UDC2_INFO, * PUDC2_INFO;
